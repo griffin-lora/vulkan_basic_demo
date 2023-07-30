6b358f65dc856543e908188db08866f81d176ef4
TrafficConeGod <trafficconegod@github.com>
2023-07-29 20:55:18 -0400

    add instance and validation layers

--- src/main.c
+++ src/main.c
@@ -1,10 +1,41 @@
-#include <stdio.h>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-static GLFWwindow* win;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "util.h"
 
 #define WIDTH 800
 #define HEIGHT 600
+
+static const char* layers[] = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+static bool check_layers() {
+    uint32_t num_available_layers;
+    vkEnumerateInstanceLayerProperties(&num_available_layers, NULL);
+    VkLayerProperties* available_layers = malloc(num_available_layers*sizeof(VkLayerProperties)); // TODO: Ugly malloc
+    vkEnumerateInstanceLayerProperties(&num_available_layers, available_layers);
+
+    // TODO: This is kinda ugly
+    for (size_t i = 0; i < NUM_ELEMS(layers); i++) {
+        bool found = false;
+        for (size_t j = 0; j < num_available_layers; j++) {
+            if (strcmp(layers[i], available_layers[j].layerName) == 0) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
     glfwInit();
@@ -12,12 +43,50 @@ int main() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    win = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", NULL, NULL);
+    GLFWwindow* win = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", NULL, NULL);
+
+    //
+    if (!check_layers()) {
+        printf("Validation layers requested, but not available\n");
+        return 1;
+    }
+
+    VkApplicationInfo app_info = {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Hello Triangle",
+        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "No Engine",
+        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = VK_API_VERSION_1_0
+    };
+
+    uint32_t num_extensions = 0;
+    const char** extensions = glfwGetRequiredInstanceExtensions(&num_extensions);
+
+    VkInstanceCreateInfo instance_create_info = {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &app_info,
+        .enabledExtensionCount = num_extensions,
+        .ppEnabledExtensionNames = extensions,
+        .enabledLayerCount = NUM_ELEMS(layers),
+        .ppEnabledLayerNames = layers
+        // RELEASE:
+        // .enabledLayerCount = 0
+    };
+    VkInstance instance;
+    if (vkCreateInstance(&instance_create_info, NULL, &instance) != VK_SUCCESS) {
+        printf("Failed to create instance\n");
+        return 1;
+    }
+
+    //
 
     while (!glfwWindowShouldClose(win)) {
         glfwPollEvents();
         // draw frame
     }
+
+    vkDestroyInstance(instance, NULL);
 
     glfwDestroyWindow(win);
     glfwTerminate();
---
+++ src/util.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define NUM_ELEMS(ARRAY) (sizeof((ARRAY))/sizeof((ARRAY)[0]))
