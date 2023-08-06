6e38a713854d9efdeeb0f21d9372bfe32167e2e5
TrafficConeGod <trafficconegod@github.com>
2023-08-05 23:33:52 -0400

    add bad frame wait

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "vk/render.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
     const char* msg = init_vulkan_core();
@@ -11,12 +12,20 @@ int main(void) {
     }
 
     while (!glfwWindowShouldClose(window)) {
+        double start = glfwGetTime();
         glfwPollEvents();
 
         msg = draw_vulkan_frame();
         if (msg != NULL) {
             printf("%s", msg);
             return 1;
+        }
+        double end = glfwGetTime();
+        double delta = end - start;
+
+        double remaining = (1.0f/60.0f) - delta;
+        if (remaining > 0.0f) {
+            usleep(remaining * 1000000);
         }
     }
 
