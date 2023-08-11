0fab6bf8cd8327b4b72317ae83c478dbb101eb5e
TrafficConeGod <trafficconegod@github.com>
2023-08-11 13:33:29 -0400

    add better input code

--- src/input.c
+++ src/input.c
@@ -9,7 +9,7 @@
 #define M_TAU (GLM_PI * 2)
 
 #define MOVE_SPEED 0.3f
-#define ROT_SPEED 1.0f
+#define ROT_SPEED 2.0f
 
 static vec3s cam_pos = {{ 1.5f, -0.5f, 1.0f }};
 static vec3s cam_vel = {{ 0.0f, 0.0f, 0.0f }};
@@ -17,38 +17,69 @@ static vec3s cam_vel = {{ 0.0f, 0.0f, 0.0f }};
 static vec2s cam_rot = {{ -2.21f, 0.21f }};
 static vec2s cam_rot_vel = {{ 0.0f, 0.0f }};
 
-static bool has_last_norm_cursor_pos = false;
-static vec2s last_norm_cursor_pos = {{ 0.0f, 0.0f }};
+static bool in_rotation_mode = false;
+static vec2s rotation_mode_cursor_position = {{ 0.0f, 0.0f }};
+static vec2s rotation_mode_norm_cursor_position = {{ 0.0f, 0.0f }};
+
+static bool is_cursor_position_out_of_bounds(vec2s cursor_position) {
+    return cursor_position.x < 0 || cursor_position.x >= swap_image_extent.width || cursor_position.y < 0.0f || cursor_position.y >= swap_image_extent.height;
+}
+
+static vec2s get_norm_cursor_position(float aspect, vec2s cursor_position) {
+    vec2s norm_cursor_position = glms_vec2_mul(cursor_position, (vec2s) {{ 1.0f/(float)swap_image_extent.width, 1.0f/(float)swap_image_extent.height }});
+    norm_cursor_position.x *= aspect;
+    return norm_cursor_position;
+}
+
+static vec2s get_desired_rotational_velocity(float aspect) {
+    int mouse_button = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2);
+
+    if (in_rotation_mode && mouse_button != GLFW_PRESS) {
+        in_rotation_mode = false;
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        return (vec2s) {{ 0.0f, 0.0f }};
+    }
+    if (!in_rotation_mode && mouse_button == GLFW_PRESS) {
+        double cursor_x;
+        double cursor_y;
+        glfwGetCursorPos(window, &cursor_x, &cursor_y);
+
+        const vec2s cursor_position = {{ cursor_x, cursor_y }};
+        if (is_cursor_position_out_of_bounds(cursor_position)) {
+            return (vec2s) {{ 0.0f, 0.0f }};
+        }
+
+        in_rotation_mode = true;
+        rotation_mode_cursor_position = cursor_position;
+        rotation_mode_norm_cursor_position = get_norm_cursor_position(aspect, cursor_position);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+        return (vec2s) {{ 0.0f, 0.0f }};
+    }
+
+    if (!in_rotation_mode) {
+        return (vec2s) {{ 0.0f, 0.0f }};
+    }
+
+    double cursor_x;
+    double cursor_y;
+    glfwGetCursorPos(window, &cursor_x, &cursor_y);
+
+    const vec2s cursor_position = {{ cursor_x, cursor_y }};
+    if (is_cursor_position_out_of_bounds(cursor_position)) {
+        return (vec2s) {{ 0.0f, 0.0f }};
+    }
+
+    glfwSetCursorPos(window, rotation_mode_cursor_position.x, rotation_mode_cursor_position.y);
+    
+    return glms_vec2_scale(glms_vec2_sub(rotation_mode_norm_cursor_position, get_norm_cursor_position(aspect, cursor_position)), ROT_SPEED);
+}
 
 void handle_input(float delta) {
     float aspect = (float)swap_image_extent.width/(float)swap_image_extent.height;
 
     mat4s projection = glms_perspective(M_TAU / 5.0f, aspect, 0.01f, 300.0f);
 
-    vec2s desired_rot_vel = {{ 0.0f, 0.0f }};
-    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS) {
-        double cursor_x;
-        double cursor_y;
-        glfwGetCursorPos(window, &cursor_x, &cursor_y);
-
-        const vec2s cursor_pos = {{ cursor_x, cursor_y }};
-        if (cursor_pos.x >= 0 && cursor_pos.x <= swap_image_extent.width && cursor_pos.y >= 0.0f && cursor_y <= swap_image_extent.height) {
-            vec2s norm_cursor_pos = glms_vec2_mul(cursor_pos, (vec2s) {{ 1.0f/(float)swap_image_extent.width, 1.0f/(float)swap_image_extent.height }});
-            norm_cursor_pos.x *= aspect;
-
-            if (has_last_norm_cursor_pos) {
-                desired_rot_vel = glms_vec2_scale(glms_vec2_sub(norm_cursor_pos, last_norm_cursor_pos), ROT_SPEED);
-                desired_rot_vel.y *= -1.0f;
-            }
-
-            has_last_norm_cursor_pos = true;
-            last_norm_cursor_pos = norm_cursor_pos;
-        } else {
-            has_last_norm_cursor_pos = false;
-        }
-    } else {
-        has_last_norm_cursor_pos = false;
-    }
+    vec2s desired_rot_vel = get_desired_rotational_velocity(aspect);
     cam_rot_vel = glms_vec2_lerp(cam_rot_vel, desired_rot_vel, 0.2f);
 
     cam_rot = glms_vec2_add(cam_rot, cam_rot_vel);
@@ -61,8 +92,6 @@ void handle_input(float delta) {
     const vec2s pitch_vector = {{ cosf(cam_rot.y), sinf(cam_rot.y) }};
     const vec2s yaw_vector = {{ cosf(cam_rot.x), sinf(cam_rot.x) }};
     vec3s cam_forward = {{ yaw_vector.x * pitch_vector.x, pitch_vector.y, yaw_vector.y * pitch_vector.x }};
-    vec3s cam_right = {{ -cam_forward.z, 0.0f, cam_forward.x }};
-    glm_normalize(cam_right.raw);
 
     vec3s input_vec = {{ 0.0f, 0.0f, 0.0f }};
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
@@ -83,7 +112,7 @@ void handle_input(float delta) {
     mat3s movement_mat = (mat3s) {{
         { cam_forward.x, cam_forward.y, cam_forward.z },
         { 0, 1, 0 },
-        { -cam_forward.z, 0, cam_forward.x }
+        { cam_forward.z, 0, -cam_forward.x }
     }};
     glm_normalize(movement_mat.col[2].raw);
 
@@ -92,7 +121,7 @@ void handle_input(float delta) {
 
     cam_pos = glms_vec3_add(cam_pos, cam_vel);
 
-    mat4s view = glms_look(cam_pos, cam_forward, (vec3s) {{ 0.0f, 1.0f, 0.0f }});
+    mat4s view = glms_look(cam_pos, cam_forward, (vec3s) {{ 0.0f, -1.0f, 0.0f }});
 
     clip_space = glms_mat4_mul(projection, view);
 
