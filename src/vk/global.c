759d9a963fae1f69591fd6c33c8cdf27707f503a
griffin-lora <36275551+griffin-lora@users.noreply.github.com>
2023-08-27 12:37:18 -0400

    remove stuff

--- src/vk/global.c
+++ src/vk/global.c
@@ -5,8 +5,6 @@
 #include <stdalign.h>
 #include <assert.h>
 
-// TODO: Fix this mess
-
 alignas(64)
 VkInstance instance;
 VkSurfaceKHR surface;
@@ -19,20 +17,16 @@ VkFence in_flight_fences[NUM_FRAMES_IN_FLIGHT];
 VkSwapchainKHR swapchain;
 VkSemaphore image_available_semaphores[NUM_FRAMES_IN_FLIGHT];
 VkFramebuffer* swapchain_framebuffers;
-// static_assert(sizeof(window) + sizeof(device) + sizeof(in_flight_fence) + sizeof(swapchain) + sizeof(image_available_semaphore) + sizeof(command_buffer) + sizeof(render_pass) + sizeof(swapchain_framebuffers) == 64, "");
 
-// alignas(64)
 VkExtent2D swap_image_extent;
 VkSemaphore render_finished_semaphores[NUM_FRAMES_IN_FLIGHT];
 VkQueue graphics_queue;
 VkQueue presentation_queue;
-// static_assert(sizeof(swap_image_extent) + sizeof(pipeline) + sizeof(render_finished_semaphore) + sizeof(graphics_queue) + sizeof(presentation_queue) == 40, "");
 
 VkCommandPool command_pool;
 VkImage* swapchain_images;
 VkImageView* swapchain_image_views;
 
-// alignas(64)
 uint32_t num_swapchain_images;
 
 VkPhysicalDevice physical_device;
